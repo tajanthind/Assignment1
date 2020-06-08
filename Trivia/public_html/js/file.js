2da87388a7d23fda90f38a185b66b9aa71100a9f
tajanthind <tajanthind01@gmail.com>
2020-06-08 00:58:54 -0400

    next Question button works. Start button added.

--- Trivia/public_html/js/file.js
+++ Trivia/public_html/js/file.js
@@ -15,8 +15,10 @@ let options = [
 
 let index = 0;
 let score = 0;
+let question = document.getElementById('question');
+let ans = document.getElementById('ans');
 
-function checkAnswer() {
+function checkAnswer(answer) {
     if (document.getElementById('ans1').checked) {
         if (options[index][0] === questions[index][1]) {
             score++;
@@ -30,6 +32,36 @@ function checkAnswer() {
             score++;
         }
     }
+}
 
+function startGame() {
+    document.getElementById("question").style.display = "block";
+    document.getElementById("ans").style.display = "block";
+    document.getElementById("buttons").style.display = "block";
+    document.getElementById("start").style.display = "none";
+    setFirstQuestion();
+}
+
+function setFirstQuestion() {
+    let r = document.getElementsByTagName("label")
+    document.getElementById("question").innerHTML = questions[index][0];
+    r[0].innerHTML = options[index][0];
+    r[1].innerHTML = options[index][1];
+    r[2].innerHTML = options[index][2];
+}
+
+function nextQuestion() {
     index++;
+    if (index == 4) {
+        document.getElementById("next").style.display = "none";
+    }
+    if (index > 5) {
+        document.getElementById("next").style.display = "none";
+    } else {
+        let r = document.getElementsByTagName("label")
+        document.getElementById("question").innerHTML = questions[index][0];
+        r[0].innerHTML = options[index][0];
+        r[1].innerHTML = options[index][1];
+        r[2].innerHTML = options[index][2];
+    }
 }
